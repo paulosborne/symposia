2a54a5481bb4e31fe7c601fc45aa3b9850900767
Paul Osborne <paul.osborne@symphony-group.co.uk>
2013-02-08 15:43:22 +0000

    issues with scope on core inteface force change

--- test/specs/app.spec.js
+++ test/specs/app.spec.js
@@ -1,21 +1,23 @@
 define(["core","./stubs/testmodule"],function ( symposia, test_module ) {
     describe("symposia - public interface", function () {
+
         it("should be able to bootstrap an application", function () {
-            expect(typeof symposia.bootstrap).toBe("function");
+            expect(typeof symposia.modules.create_multiple).toBe("function");
         });
 
         it("should throw an exception if symposia.bootstrap isn't passed an object", function () {
-            var spy = sinon.spy(symposia,'bootstrap');
+            var spy = sinon.spy(symposia.modules,'create_multiple');
 
             expect(function () {
-                symposia.bootstrap('this should throw exception');
+                symposia.modules.create_multiple('this should throw exception');
             }).toThrow('object expected');
         });
 
         it("should be able to create multiple modules", function () {
-            var spy = sinon.spy('symposia','bootstrap');
+            var spy = sinon.spy(symposia.modules,'create');
+            var result;
 
-            symposia.bootstrap({
+            symposia.modules.create_multiple({
                 'notes': {
                     element: '#notes',
                     creator: test_module
@@ -24,13 +26,14 @@ define(["core","./stubs/testmodule"],function ( symposia, test_module ) {
                     element: '#history',
                     creator: test_module
                 }
+            }, function ( result ) {
+                expect(result).toContain('notes');
+                expect(result.length).toBe(2);
             });
 
-        });
+            expect(spy.called).toBeTruthy();
+            expect(spy.withArgs('notes', test_module, '#notes')).toBeTruthy();
 
-        it("should be able to audit modules", function () {
-            var modules = symposia.modules.list();
-            expect(typeof modules).toBe('object');
         });
     });
 });
