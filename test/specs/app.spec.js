f17863db16741516b6a14028fcb4ed0fef365053
Paul Osborne <paul.osborne@symphony-group.co.uk>
2013-02-08 16:20:01 +0000

    Trimmed combined create and create_multiple into one method

--- test/specs/app.spec.js
+++ test/specs/app.spec.js
@@ -2,22 +2,20 @@ define(["core","./stubs/testmodule"],function ( symposia, test_module ) {
     describe("symposia - public interface", function () {
 
         it("should be able to bootstrap an application", function () {
-            expect(typeof symposia.modules.create_multiple).toBe("function");
+            expect(typeof symposia.bootstrap).toBe("function");
         });
 
-        it("should throw an exception if symposia.bootstrap isn't passed an object", function () {
-            var spy = sinon.spy(symposia.modules,'create_multiple');
+        it("should throw an exceptiontion", function  () {
+            var spy = sinon.spy(symposia,'bootstrap');
 
             expect(function () {
-                symposia.modules.create_multiple('this should throw exception');
+                symposia.bootstrap('this should throw exception');
             }).toThrow('object expected');
         });
 
         it("should be able to create multiple modules", function () {
-            var spy = sinon.spy(symposia.modules,'create');
-            var result;
 
-            symposia.modules.create_multiple({
+            symposia.bootstrap({
                 'notes': {
                     element: '#notes',
                     creator: test_module
@@ -26,13 +24,10 @@ define(["core","./stubs/testmodule"],function ( symposia, test_module ) {
                     element: '#history',
                     creator: test_module
                 }
-            }, function ( result ) {
-                expect(result).toContain('notes');
-                expect(result.length).toBe(2);
+            }, function ( moduleData ) {
+                expect(typeof moduleData).toBe('object');
             });
 
-            expect(spy.called).toBeTruthy();
-            expect(spy.withArgs('notes', test_module, '#notes')).toBeTruthy();
 
         });
     });
