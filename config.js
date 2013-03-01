516d0897fdd9fc25a7ed4cb481ec96f6127ad694
Paul Osborne <paul.osborne@symphony-group.co.uk>
2013-03-01 13:05:13 +0000

    Updated some core components

--- config.js
+++ config.js
@@ -1,5 +1,20 @@
-define(function() {
-    require.config({
-        baseUrl: "./"
-    });
+require.config({
+    baseUrl: "/js/vendor/symposia",
+    packages: [
+        'app/modules/document_toolbar',
+        'app/modules/document_viewer'
+        ],
+    shim: {
+        'backbone': {
+            exports: 'Backbone',
+            deps: ['underscore']
+        }
+    },
+    paths: {
+        underscore: 'vendor/underscore-1.4.3',
+        backbone: 'vendor/backbone-0.0.9',
+        jquery: 'vendor/jquery-1.8.3'
+    }
 });
+
+require(['app/main']);
