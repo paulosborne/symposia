7d216687925c6e9b9ff9ec74e4bd5fe8643a5f84
Paul Osborne <paulosborne@Pauls-MacBook-Pro.local>
2013-05-01 22:49:01 +0100

    Updated dependencies

--- symposia/sandbox.js
+++ symposia/sandbox.js
@@ -1,4 +1,4 @@
-define(['symposia/lib/jquery'], function ( $ )  {
+define(['jquery'], function ( $ )  {
     return {
         create: function ( symposia, selector ) {
             var element = $('#' + selector);
