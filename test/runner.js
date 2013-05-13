449d03af1c6823692af5466d1f7cd1ccae9a16aa
Paul Osborne <paul.osborne@symphony-group.co.uk>
2013-05-13 11:27:59 +0100

    Adding mocha + chai + sinon as testing solution

---
+++ test/runner.js
@@ -0,0 +1,29 @@
+require.config({
+    baseUrl: '../',
+    paths: {
+        'symposia': 'vendor/symposia/symposia',
+        'chai': 'node_modules/chai/chai',
+        'sinonChai': 'node_modules/sinon-chai/lib/sinon-chai'
+    }
+});
+
+require(['chai','sinonChai'], function ( chai, sinonChai ) {
+    window.chai = chai;
+    window.expect = chai.expect;
+    window.assert = chai.assert;
+    window.should = chai.should();
+    window.sinonChai = sinonChai;
+    window.notrack = true;
+
+    chai.use(sinonChai);
+    mocha.setup('tdd');
+
+    require([
+        'test/specs/symposia.spec'
+    ], function () {
+        mocha.run();
+    });
+
+}, function ( err ) {
+    console.log ( err );
+});
