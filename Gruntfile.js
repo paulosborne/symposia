0e37992b3ab90ac0f9b927abcf6406580df77264
Paul Osborne <paul.osborne@symphony-group.co.uk>
2013-05-14 15:33:23 +0100

    Minor update

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,8 +30,7 @@ module.exports = function(grunt) {
     mocha: {
         all: {
             options: {
-                urls: ['http://localhost:<%= connect.server.options.port %>/test/index.html'],
-                reporter: 'Spec'
+                urls: ['http://localhost:<%= connect.server.options.port %>/test/index.html']
             }
         }
     },
