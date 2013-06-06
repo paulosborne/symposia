3ac04d51e4cfb97804af1fc206f32242676b4668
posbo <paul.osborne.uk@gmail.com>
2013-06-06 23:42:39 +0100

    #6 Minor update

--- src/core.js
+++ src/core.js
@@ -1,16 +1,17 @@
 define(['src/base'], function( base ) {
 
-    var core = {
-        _modules   : {},
-        _sandboxes : {}
-    };
+    var core = {};
+
+    // contains module state.
+    core._modules = {};
+
+    // contains sandbox state.
+    core._sandboxes = {};
 
     if ( !base.hasOwnProperty('bus') ) {
         throw new Error('Base object must contain a message bus');
     }
 
-    _.extend( core, base );
-
-    return core;
+    return _.extend( core, base );
 
 });
--- symposia.js
+++ symposia.js
@@ -1,3 +1,6 @@
 define(['src/base','src/core','src/sandbox','src/event','src/module'], function ( base, core ) {
+
+    window.symposia = core;
+
     return core;
 });
