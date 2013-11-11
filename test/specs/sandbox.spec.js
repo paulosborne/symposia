b47d2d481c3718605d528229acfc8731760b29d3
Paul Osborne <paul.osborne.uk@gmail.com>
2013-11-11 00:19:37 +0000

    Rewriting sandbox spec

--- test/specs/sandbox.spec.js
+++ test/specs/sandbox.spec.js
@@ -1,92 +1,58 @@
-define(['symposia'], function ( symposia ) {
-    describe('Core.Sandbox', function () {
+define(function (require) {
 
-        var sandboxes = [];
+    var symposia    = require('symposia');
 
-        before(function () {
-            sandboxes.push( symposia.sandbox.create( 'todo' ));
-            sandboxes.push( symposia.sandbox.create( 'module_a'));
-            sandboxes.push( symposia.sandbox.create( 'module_b'));
-        });
+    suite('Sandbox', function () {
 
-        it('should return a new sandbox', function () {
-            _.each( sandboxes, function ( sandbox ) {
-                assert.isObject( sandbox );
-                assert.property( sandbox,'publish');
-                assert.property( sandbox,'subscribe');
+        var sandboxes   = [],
+            container   = document.getElementById('container'),
+            tagName     = 'div',
+            total         = 100;
+
+        suiteSetup(function () {
+            _(total).times(function (i) {
+                var div = document.createElement(tagName),
+                    _id = _.uniqueId('module-');
+
+                div.id = _id;
+                container.appendChild(div);
+                sandboxes.push( symposia.sandbox.create(_id) );
             });
         });
 
-        it('should assign each sandbox a unique name', function () {
-            _.each( sandboxes, function ( sandbox ) {
-                assert.match( sandbox.getId(), /^sandbox-[0-9]/);
+        suite('#create', function () {
+            test("should create sandboxes", function () {
+                sandboxes.should.have.length(total);
             });
         });
 
-        it('should attempt to find a DOM element matching the second parameter', function () {
-            var $el = sandboxes[0].getElement();
-            assert.isString( $el.jquery );
-            assert.equal( $el.prop('nodeName'), 'DIV' );
+        suite('#getId', function () {
+            test("should return the unique sandbox id", function () {
+                _.each(sandboxes, function (sandbox) {
+                    expect(sandbox.getId()).to.match(/sandbox-[0-9]+/g);
+                });
+            });
         });
 
-        it('passing a selector to getElement finds child elements of the module container', function () {
-            var $todo       = sandboxes[0].getElement('h1'),
-                $module_a   = sandboxes[1].getElement('h1'),
-                $module_b   = sandboxes[2].getElement('h1');
+        suite('#getElement', function () {
+            test('should find matching DOM elements', function () {
+                _.each(sandboxes, function (sandbox) {
+                    var node = sandbox.getElement();
 
-            assert.equal($todo.length, 1);
-            assert.equal($todo.text(),'todo');
-
-            assert.equal($module_a.length, 1);
-            assert.equal($module_a.text(),'module_a');
-
-            assert.equal($module_b.length, 1);
-            assert.equal($module_b.text(),'module_b');
-
+                });
+            });
         });
 
-            describe('subscribe()', function () {
-
-                var callback = sinon.spy();
-
-                before(function () {
-
-                    sandboxes[0].subscribe({
-                        topic: 'menu.click',
-                        callback: callback
-                    });
-
+        suite('#getSubscriptions', function () {
+            suiteSetup(function () {
+                _.each(sandboxes, function (sandbox) {
+                    var callbackSpy = sinon.spy();
+                    sandbox.subscribe({ topic: 'ping', callback: callbackSpy });
                 });
-
-                it('should be able to publish messages', function () {
-                    sandboxes[1].publish({ topic: 'menu.click', data: { opt: 'zoom' }});
-                    assert.propertyVal( callback.args[0][0],'sender', sandboxes[1].getId() );
-                });
-
-                it('should be able to subscribe to published messages', function () {
-                    assert.lengthOf(callback.args[0],2);
-                    assert.propertyVal( callback.args[0][0],'opt','zoom');
-                });
-
-                it('should return the subscriptions made by the sandbox instance', function () {
-                    assert.lengthOf( sandboxes[0].getSubscriptions(), 1 );
-                });
-
             });
+        });
 
-            describe('addWireTap', function () {
-                var callback = sinon.spy();
-
-                it('should fire callback when activity is observed', function () {
-                    sandboxes[0].addWireTap( callback );
-                    sandboxes[0].publish({ topic: 'menu.click' });
-                    assert.isTrue( callback.called );
-                });
-
-            });
-
-        after(function () {
-            sandboxes = null;
+        suiteTeardown(function () {
         });
     });
 });
