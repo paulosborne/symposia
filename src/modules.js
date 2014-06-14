18de3bef2f6e2bbc93c30e7bee892592aef89079
Paul Osborne <paul.osborne@symphony-group.co.uk>
2014-06-14 10:10:40 +0100

    initial commit of modules

--- src/modules.js
+++ src/modules.js
@@ -1,31 +1,83 @@
-module.exports = function  (sym, util) {
-    var _modules = {};
+var _ = require('underscore');
+
+function symposiaModule (sym) {
     var api = {};
-    
-    var ModuleDefinition = function (name, fn) {
-        this.fn = fn;
-        this.name = name;
+    var _modules = {};
+    var strings = sym.config.strings;
 
-        return this;
+    /**
+     * Create a module
+     *
+     * @param {string} name
+     * @param {function} fn
+     */
+    api.create = function (name, fn) {
+        var module, test, sandbox;
+
+        if (!_.isFunction(fn)) {
+            throw new Error(strings.errors.ERROR_CREATING_MODULE);
+        }
+
+        test = fn(sym.sandbox.create(name));
+
+        if (!_.isObject(test) || !_.has(test, 'init')) {
+            throw new Error(strings.errors.ERROR_INITIALIZING_MODULE);
+        }
+
+        _modules[name] = {
+            id: _.uniqueId('module_'),
+            seed: fn,
+            createdAt: new Date(),
+        };
+
+        return _modules[name];
     };
 
-    ModuleDefinition.prototype.start = function () {
-        this.sandbox = sym.sandbox.create();
+    /**
+     * Start a module
+     *
+     * @param {string} name - module to start
+     */
+    api.start = function (name) {
+        if(!_.has(_modules, name) || _.has(_modules[name], 'instance')) {
+            return false;
+        }
+
+        _modules[name].instance = _modules[name].seed(sym.sandbox.create(name));
     };
 
-    api.create = function () {
-        return;
+    api.stop = function (name) {
+        console.log('stopping', name);
     };
 
-    api.start = function() {
-        return;
+    /**
+     * Destroys a module
+     *
+     * @param {string} name - module to destroy
+     */
+    api.destroy = function (name) {
+        if (!_.has(_modules, name)) {
+            return false;
+        }
+
+        if (_.has(_modules[name],'instance')) {
+            this.stop(name);
+        }
+
+        delete(_modules[name]);
     };
 
-    api.stop = function () {
-        return;
+    api.destroyAll = function () {
+        _.each(_modules, function (obj, name) {
+            this.destroy(name);
+        }, this);
     };
 
-    // expose modules API
+    api.getModules = function () {
+        return _modules;
+    };
+
     sym.modules = api;
+}
 
-};
+module.exports = symposiaModule;
