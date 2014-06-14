ba2abfb1f19f159aba8edf784d16f27548832324
Paul Osborne <paul.osborne@symphony-group.co.uk>
2014-06-14 10:17:44 +0100

    added mock modules

--- test/mocks/modules.js
+++ test/mocks/modules.js
@@ -1,51 +1,30 @@
-define(function() {
+/* global Symposia */
+var Mocks = {};
+
+Mocks.withPublish = function (sandbox) {
     return {
-        'a': function ( sandbox ) {
-            return {
-                init: sinon.spy( function () {}),
-                destroy: sinon.spy( function() {
-                }),
-                notify: sinon.spy(function () {
-                    sandbox.publish({
-                        topic: 'document.view',
-                        data: {
-                            status: 200
-                        }
-                    });
-                })
-            };
-        },
-        'b': function ( sandbox ) {
-            return {
-                init: sinon.spy(function() {}),
-                destroy: sinon.spy(function() {}),
-                listen: sinon.spy(function( callback ) {
-                    sandbox.subscribe({
-                        topic: 'document.view',
-                        callback: callback
-                    });
-                })
-            };
-        },
-        'c': function ( sandbox ) {
-            return {
-                init: sinon.spy(),
-                destroy: sinon.spy()
-            };
-        },
-        /**
-         * call sandbox from outside public interface ( issue #2 )
-         */
-        'd': function ( sandbox ) {
-            sandbox.addWireTap(function( data ) { console.log( data ) });
-            return {
-                init: sinon.spy(),
-                destroy: sinon.spy()
-            }
-        },
-        'e': function (sandbox) {
-            return {
-            }
-        }
+        init: sinon.spy(),
+        destroy: sinon.spy(),
+        notify: sinon.spy(function () {
+            sandbox.publish({
+                topic: 'test.message',
+                data: { status: 200 }
+            });
+        })
     };
-});
+};
+
+Mocks.withSubscribe = function (sandbox) {
+    return {
+        init: sinon.spy(),
+        destroy: sinon.spy(),
+        listen: sinon.spy(function() {
+            sandbox.subscribe({
+                topic: 'test.message',
+                callback: sinon.spy()
+            });
+        })
+    };
+};
+
+Symposia.Mocks = Mocks;
