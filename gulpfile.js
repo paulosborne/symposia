b07bddfd8362472ee2f7f9bc556baeaf12d47829
Paul Osborne <paul.osborne@symphony-group.co.uk>
2014-06-14 10:18:52 +0100

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,22 @@
-var browserify  = require('browserify');
-var watchify    = require('gulp-watchify');
-var streamify   = require('gulp-streamify');
 var gulp        = require('gulp');
-var path        = require('path');
+var browser     = require('open');
+var browserify  = require('browserify');
 var clean       = require('gulp-clean');
 var gutil       = require('gulp-util');
-var mocha       = require('gulp-mocha-phantomjs');
 var buffer      = require('gulp-buffer');
 var rename      = require('gulp-rename');
 var uglify      = require('gulp-uglify');
-var express     = require('express');
+var watch       = require('gulp-watch');
 var source      = require('vinyl-source-stream');
-var port        = 8000;
-var servers;
-var watching    = false;
 
-gulp.task('enable-watch-mode', function () {
-    watching = true;
-});
+var paths       = {
+    scripts: ['./index.js','./src/**/*.js']
+};
 
-gulp.task('dist', function () {
-    var bundleStream = browserify('./index.js').bundle({
-        standalone: 'Symposia'
-    });
+gulp.task('browserify', function () {
+    var b = browserify('./index.js');
 
-    bundleStream
+    b.bundle({ standalone: 'Symposia' })
     .pipe(source('symposia.js'))
     .pipe(buffer())
     .pipe(gulp.dest('./dist'))
@@ -39,42 +31,8 @@ gulp.task('clean', function () {
     .pipe(clean());
 });
 
-gulp.task('build', watchify(function (watchify) {
-    return gulp.src('./src/symposia.js')
-    .pipe(watchify({
-        watch: watching,
-        standalone: 'Symposia'
-    }))
-    .pipe(streamify(uglify()))
-    .pipe(gulp.dest('./dist'));
-}));
-
-gulp.task('test', function () {
-    return gulp.src('./test/index.html')
-    .pipe(mocha({ reporter: 'nyan', dump: './test.xml' }));
+gulp.task('watch', function () {
+    gulp.watch(paths.scripts, ['clean','browserify']);
 });
 
-var createServer = function (port) {
-    var p = path.resolve("./");
-    var app = express();
-
-    app.use(express.static(p));
-    app.listen(port, function () {
-        gutil.log('listening on ', port);
-    });
-
-    return {
-        app: app
-    };
-};
-
-gulp.task("server", function () {
-    if (!servers) {
-        servers = createServer(port);
-    }
-
-    open("http://localhost:"+ port +"/index.html");
-});
-
-gulp.task('default', ['clean','build']);
-gulp.task('watchify', ['enable-watch-mode','clean','build']);
+gulp.task('default', ['clean','browserify']);
