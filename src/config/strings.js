48a95ecb035d3c9f38c9adb412be6020afdb621a
Paul Osborne <paul.osborne@symphony-group.co.uk>
2014-06-18 11:54:00 +0100

    rethink of config/strings

---
+++ src/config/strings.js
@@ -0,0 +1,6 @@
+module.exports = {
+    // ERRORS
+    ERROR_INITIALIZING_MODULE: "Unable to initialize module",
+    ERROR_CREATING_MODULE: "Unable to create module",
+    ERROR_NO_MODULES: "A valid module definition must be supplied"
+};
--- src/config/strings/errors.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    // Modules
-    ERROR_INITIALIZING_MODULE: "Unable to initialize module",
-    ERROR_CREATING_MODULE: "Unable to create module"
-};
