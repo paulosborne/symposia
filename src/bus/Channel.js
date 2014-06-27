2941564e05ec67536d4c27ab68d977984e7eafa8
Paul Osborne <paul.osborne@symphony-group.co.uk>
2014-06-27 22:55:14 +0100

    Added channel constructor

---
+++ src/bus/Channel.js
@@ -0,0 +1,26 @@
+function Channel () {
+    return this;
+}
+
+/**
+ * Adds a subscription to this channel
+ *
+ * @param {object} subscr - object containing subscription properties
+ * @return Channel
+ */
+Channel.prototype.addSubscription = function (subscr) {
+
+    var topic   = subscr.topic;
+    var sid     = subscr.sid;
+    var cb      = subscr.callback;
+
+    this[topic] = this[topic] || {};
+    this[topic][sid] = this[topic][sid] || {};
+    this[topic][sid].callbacks = this[topic][sid].callbacks || [];
+    this[topic][sid].callbacks.push(cb);
+
+    return this;
+
+};
+
+module.exports = Channel;
