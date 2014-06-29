b3919595689d0a71781d302fed0df479a849adb3
Paul Osborne <paul.osborne@symphony-group.co.uk>
2014-06-29 21:44:22 +0100

    updates to sandbox

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,142 +1,46 @@
 var _ = require('underscore');
 
-function Sandbox (sym) {
-    var _subscriptions = {};
-    var regex = {};
+function Sandbox (symposia) {
+    var sandbox = {};
 
-    var SandboxDefinition = function (name) {
-        var _el = sym.dom.find('#'+ name);
+    // var el = symposia.dom.find('#'+ name);
+
+    sandbox.create = function (name) {
         var _id = _.uniqueId('sandbox_');
-
-        function compare (binding, topic) {
-            var prev, pattern, rgx;
-
-            if (!(rgx = regex[binding])) {
-                pattern = "^"+ binding.split('.').map(function (segment) {
-                    var res = (!!prev) ? "\\.\\b" : "";
-
-                    res += (segment === '*') ? "[^.+]" : segment;
-                    prev = segment;
-
-                    return res;
-                }).join("") + '$';
-
-                rgx = regex[binding] = new RegExp(pattern);
-            }
-            return rgx.test(topic);
-        }
-
+        
         return {
-            id: _id,
-            el: _el,
-            find: function (str) {
-                return _el ? sym.dom.find(str, _el) : sym.dom.find(str);
+            getSubscriptions: function () {
+                 return symposia.bus.getBySubscriberId(_id);
             },
             /**
-             * Creates a new event subscription
-             *
-             * @param {object}  def subscription definition
-             */
-            subscribe: function (def) {
-                var channel = def.channel || sym.config.channel.DEFAULT;
-                var topic   = def.topic;
-
-                _subscriptions[channel] = _subscriptions[channel] || {};
-                _subscriptions[channel][topic] = _subscriptions[channel][topic] = {};
-                _subscriptions[channel][topic][_id] = _subscriptions[channel][topic][_id] = [];
-                _subscriptions[channel][topic][_id].push(def.callback);
-
-                return;
-            },
-            /**
-             * Publish an event
+             * Publish a message
              *
              * @param {object} envelope
              */
-            publish: function (envelope) {
-                var self = this;
-                envelope.channel = envelope.channel || sym.config.channels.DEFAULT;
-                envelope.timeStamp = new Date();
-
-                if (_subscriptions[envelope.channel]) {
-                    _.each(_subscriptions[envelope.channel], function (sandboxes, topic) {
-                        if(compare(topic, envelope.topic)) {
-                            _.each(sandboxes, function (callbacks, sandbox) {
-                                _.each(callbacks, function (cb) {
-                                    cb.call(null, envelope.data, envelope);
-                                });
-                            });
-                        }
-                    });
-                }
-
-                return;
+            publish: function (message) {
+                 return symposia.bus.publish(message);
             },
             /**
-             * Remove a subscription
+             * Create a new subscription
              *
-             * @param {object} def
+             * @param {object} subscription
              */
-            unsubscribe: function (def) {
-                var channel = def.channel || sym.config.channels.DEFAULT;
-                var topic = def.topic;
+            subscribe: function (subscription) {
+                subscription.sid = _id;
 
-                if (!_subscriptions[channel] || !_subscriptions[channel][topic]) {
-                    return;
-                }
-
-                delete(_subscriptions[channel][topic][_id]);
-
-                return;
+                symposia.bus.subscribe(subscription);
             },
             /**
-             * Remove all subscriptions
+             * Unsubscribe a subscription
              *
-             * @return {number}
+             * @param {topic}
              */
-            unsubscribeAll: function () {
-                var subs = this.subscriptions();
-
-                _.each(subs, function (topics, channel) {
-                    _.each(topics, function (topic) {
-                        this.unsubscribe({ channel: channel, topic: topic});
-                    }, this);
-                }, this);
-
-                return;
-            },
-            /**
-             * Returns an hash of channel subscriptions
-             *
-             * @return {object} subs
-             */
-            subscriptions: function () {
-                var subs = {};
-
-                _.each(_subscriptions, function (topics, channel) {
-                     _.each(topics, function (sandboxes, topic) {
-                         _.each(sandboxes, function (callbacks, sandbox) {
-                             if (sandbox === _id) {
-                                subs[channel] = subs[channel] = [];
-                                subs[channel].push(topic);
-                             }
-                        });
-                     });
-                });
-
-                return subs;
-            }
+            unsubscribe: function () {},
+            find: function ()  {}
         };
     };
-
-    sym.sandbox = {
-        create: function (name) {
-            return new SandboxDefinition(name);
-        },
-        getSubscriptions: function () {
-            return _subscriptions;
-        }
-    };
+    
+    symposia.sandbox = sandbox;
 }
 
 module.exports = Sandbox;
