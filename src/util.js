114fd1e00dd1866758fc5ab91f6f135ffbedc1e2
Paul Osborne <paul.osborne@symphony-group.co.uk>
2014-10-15 11:55:08 +0100

    Added utils.js

---
+++ src/util.js
@@ -0,0 +1,16 @@
+module.exports = function (symposia) {
+    var util = {};
+
+    util.generate_uuid = function () {
+        var d = new Date().getTime();
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (d + Math.random()*16)%16 | 0;
+            d = Math.floor(d/16);
+            return (c=='x' ? r : (r&0x7|0x8)).toString(16);
+        });
+
+        return uuid;
+    };
+
+    symposia.util = util;
+};
