0c861a48989c015379cca5261b71d6a04984b296
Paul Osborne <paul.osborne@symphony-group.co.uk>
2014-10-15 16:15:36 +0100

    Updated store.js

--- src/store.js
+++ src/store.js
@@ -1,31 +1,55 @@
+var EventEmitter = require('events').EventEmitter;
+
 module.exports = function (symposia) {
-    var _store      = [];
-    var _sandbox    = symposia.sandbox.create();
-    var api         = {};
+    var _       = symposia.util;
+    var _stores = {};
+    var api     = {};
 
-    api.add = function (obj) {
+    var CHANGE_EVENT    = 'change';
+    var RESET_EVENT     = 'reset';
+    var REMOVE_EVENT    = 'remove';
 
-        _store.push(obj);
+    function Store (data) {
+        this._id = _.uuid();
+        this.length = 0;
+        this._data = [];
 
-        _sandbox.publish({
-            channel: '_store',
-            topic: 'item.added',
-            data: [obj, _store]
-        });
+        if (data && _.isType(data, 'array')) {
+            this.reset(data);
+        }
 
-        return _store;
-    };
+        return this;
+    }
 
-    api.remove = function () {
-        
-    };
+    Store.prototype = _.extend(EventEmitter.prototype, {
+        set: function (obj) {
+            this._data.push(obj);
+            this.emit(CHANGE_EVENT, this);
+        },
+        /**
+         * Replace the current data set
+         * @param {array} data
+         */
+        reset: function (data) {
+            this._data = data;
+            this.length = data.length;
+            this.emit(RESET_EVENT, this);
+        },
+        /**
+         * Iterate over each data item
+         * @param {function} callback
+         */
+        each: function (callback) {
+            if (!_.isType(callback, 'function')) {
+                throw new TypeError('callback is not a method');
+            }
+            return this._data.forEach(callback);
+        }
+    });
 
-    api.list = function () {
-        return _store;
-    };
 
-    api.find = function () {
-        
+    api.create = function (initialData) {
+        return new Store(initialData);
     };
 
     symposia.store = api;
